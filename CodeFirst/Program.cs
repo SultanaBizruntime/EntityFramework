e0c5eb16e0e568d9d90a60fdcc29373af28afc3f
Sultana Bizruntime <sultana.k@bizruntime.net>
2018-03-27 10:48:51 +0530

    Entity Framework

---
+++ CodeFirst/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstDemo
+{
+   public class Posts
+   {
+        public int PostID { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+   }
+    public class CodeDbContext : DbContext
+    {
+        public DbSet<Posts> Posts { get; set; }
+    }
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            var context = new CodeDbContext();
+            var post = new Posts()
+            {
+                PostID=1,
+                Title="Title",
+                Body="Body"
+            };
+            context.Posts.Add(post);
+            context.SaveChanges();
+        }
+    }
+}
+
+
+
+
+
+//after that we connect to the sql server then in the powershell we can type
+//enable-migrations after that
+//we can create (or) modify anything for that we can type
+//add-migration CreatePost(Tablename)
---
+++ DataFirst/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new DataFirstModelEntities();
+            var post = new Post()
+            {
+                Id = 1,
+                Fname = "sulu",
+                Age = 21
+            };
+
+            context.Posts.Add(post);
+            context.SaveChanges();
+            Console.WriteLine("done");
+            Console.Read();
+        }
+    }
+}
